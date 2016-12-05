fef1a956d9ff0e2c1f6ea37fc918e6ee13508f68
taraxacum666 <taraxacum666@gmail.com>
2016-12-05 17:15:51 +0300

    start
    start

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Инвент-пром - производство и оптовая продажа мебельной фурнитуры</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container header">
+    		<img src="img/logo.png" alt="logo">
+    		<ul class="nav nav-pills">
+			  <li role="presentation" class="active"><a href="#">Главная</a></li>
+			  <li role="presentation"><a href="#">Каталог</a></li>
+			  <li role="presentation"><a href="#">О нас</a></li>
+			  <li role="presentation"><a href="#">Контакты</a></li>
+			</ul>
+			<form class="navbar-form navbar-right" role="search">
+			  <div class="form-group">
+			    <input type="text" class="form-control" placeholder="Search" id="header-search">
+			  </div>
+			  <button type="submit" class="btn btn-default">Submit</button>
+			</form>
+	</div>
+    <div class="container slider">
+    	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="1"></li>
+		    <li data-target="#myCarousel" data-slide-to="2"></li>
+		    <li data-target="#myCarousel" data-slide-to="3"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="img/slider_1.png" alt="Chania">
+		      <p>Good</p>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, velit quod aperiam cupiditate maxime, aut labore, esse nostrum tempora perferendis dignissimos perspiciatis tenetur aliquid at illo saepe, in fugit repellat.</p>
+		      <input type="button">
+		    </div>
+
+		    <div class="item">
+		      <img src="img_chania2.jpg" alt="Chania">
+		    </div>
+
+		    <div class="item">
+		      <img src="img_flower.jpg" alt="Flower">
+		    </div>
+
+		    <div class="item">
+		      <img src="img_flower2.jpg" alt="Flower">
+		    </div>
+		  </div>
+
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>	
+    </div>
+    	<div class="ourPreimn">
+    		
+    	</div>
+    	<div class="ourtop3">
+    		
+    	</div>
+    	<div class="offer">
+    		
+    	</div>
+    	<div class="footer">
+    		
+    	</div>
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
